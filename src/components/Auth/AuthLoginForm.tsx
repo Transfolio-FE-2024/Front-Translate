2d6e50513e62ee038823553b5ad15d54aee4ab6c
sstipdev <whitecandy777@naver.com>
2023-11-08 17:33:49 +0900

    [Style] - 로그인 페이지 퍼블리싱 중간작업

--- src/components/Auth/AuthLoginForm.tsx
+++ src/components/Auth/AuthLoginForm.tsx
@@ -1,21 +1,40 @@
+import styles from "./AuthLoginForm.module.scss";
 import AuthTitle from "@components/Title/AuthTitle";
 
 export default function AuthLoginForm() {
   return (
-    <>
+    <section className={styles.authSection}>
       <AuthTitle mainTitle="Login" subTitle="로그인" />
 
-      <form>
-        <label htmlFor="id">아이디</label>
-        <input id="id" type="text" placeholder="아이디를 입력해 주세요" />
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="비밀번호를 입력해 주세요"
-        />
-        <button type="submit">로그인</button>
+      <form className={styles.authForm}>
+        <div className={styles.authContainer}>
+          <label htmlFor="id" className={styles.authLabelId}>
+            아이디
+          </label>
+          <input
+            id="id"
+            type="text"
+            placeholder="아이디를 입력해 주세요"
+            className={styles.authInputId}
+          />
+        </div>
+
+        <div className={styles.authContainer}>
+          <label htmlFor="password" className={styles.authLabelPw}>
+            비밀번호
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+            className={styles.authInputPw}
+          />
+        </div>
+
+        <button type="submit" className={styles.authSubmitBtn}>
+          로그인
+        </button>
       </form>
-    </>
+    </section>
   );
 }
--- src/components/Oauth/SNSAuthList.tsx
+++ src/components/Oauth/SNSAuthList.tsx
@@ -1,14 +1,16 @@
-import KakaoBtn from "@/components/Oauth/KakaoBtn";
+import styles from "./SNSAUthList.module.scss";
+
+import KakaoBtn from "./KakaoBtn";
 import FacebookBtn from "./FacebookBtn";
 import InstarBtn from "./InstarBtn";
 
 export default function SNSAuthList() {
   return (
-    <>
-      <h1>SNS로 시작하기</h1>
+    <div className={styles.snsAuth}>
+      <h1 className={styles.snsAuthTitle}>SNS로 시작하기</h1>
       <KakaoBtn />
       <FacebookBtn />
       <InstarBtn />
-    </>
+    </div>
   );
 }
