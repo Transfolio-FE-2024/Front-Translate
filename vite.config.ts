0de74e4ce6e400b151349484be825a67bdbdd208
Yena-Yun <cheryleduart@gmail.com>
2023-11-08 17:12:20 +0900

    Setting: svg, png 타입스크립트 및 vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: [
-      { find: "@", replacement: "/src" },
-      { find: "@api", replacement: "/src/api" },
-      { find: "@components", replacement: "/src/components" },
-      { find: "@hooks", replacement: "/src/hooks" },
-      { find: "@pages", replacement: "/src/pages" },
-      { find: "@store", replacement: "/src/store" },
-      { find: "@types", replacement: "/src/types" },
-      { find: "@util", replacement: "/src/util" },
+      { find: '@', replacement: '/src' },
+      { find: '@api', replacement: '/src/api' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@store', replacement: '/src/store' },
+      { find: '@types', replacement: '/src/types' },
+      { find: '@util', replacement: '/src/util' },
     ],
   },
+  plugins: [react(), svgrPlugin()],
 });
