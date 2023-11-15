c359b21c4566d33811a16afe053ae4a8a9f90a99
Yena-Yun <cheryleduart@gmail.com>
2023-11-15 15:57:52 +0900

    feat: 포트폴리오 페이지 라우터 연결

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -3,6 +3,7 @@ import Complete from './sign-up/pages/complete/Complete';
 import Interests from './sign-up/pages/interests/Interests';
 import RegForm from './sign-up/pages/reg-from/RegForm';
 import Home from './Home/Home';
+import Portfolio from './Portfolio/Portfolio';
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
       <Route path='/signup/regform' element={<RegForm />} />
       <Route path='/signup/interests' element={<Interests />} />
       <Route path='/signup/complete' element={<Complete />} />
+      <Route path='/mypage/portfolio' element={<Portfolio />} />
     </Routes>
   );
 }
