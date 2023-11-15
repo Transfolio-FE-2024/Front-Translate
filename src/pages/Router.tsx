beae0ec900dd58e48aae012333709b88450b7e54
Yena-Yun <cheryleduart@gmail.com>
2023-11-15 13:43:53 +0900

    chore: 쌍따옴표 -> 홑따옴표 (import문)

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import { Layout } from '@/components/Layout/Layout';
-import Complete from "./sign-up/pages/complete/Complete";
-import Interests from "./sign-up/pages/interests/Interests";
-import RegForm from "./sign-up/pages/reg-from/RegForm";
+import { Routes, Route } from 'react-router-dom';
+import Complete from './sign-up/pages/complete/Complete';
+import Interests from './sign-up/pages/interests/Interests';
+import RegForm from './sign-up/pages/reg-from/RegForm';
 import Home from './Home/Home';
 
 export default function Router() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path="/signup/regform" element={<RegForm />} />
-      <Route path="/signup/interests" element={<Interests />} />
-      <Route path="/signup/complete" element={<Complete />} />
+      <Route path='/signup/regform' element={<RegForm />} />
+      <Route path='/signup/interests' element={<Interests />} />
+      <Route path='/signup/complete' element={<Complete />} />
     </Routes>
+  );
 }
