c9789cf623701082d85d22db1279c43bf5c73775
Yena-Yun <cheryleduart@gmail.com>
2023-11-23 16:01:10 +0900

    Feat: Header에 home 경로 이동 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.scss';
 import { logoIcon, menuIcon } from './icons';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
       <div className={styles.menuIcon}>
         <img src={menuIcon} />
       </div>
-      <div className={styles.logoIcon}>
+      <div className={styles.logoIcon} onClick={() => navigate('/')}>
         <img src={logoIcon} />
       </div>
     </div>
