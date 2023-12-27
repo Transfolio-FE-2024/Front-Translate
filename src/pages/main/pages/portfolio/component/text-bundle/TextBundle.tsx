b286c0b18bd714be49a621bcd08335b86d01815a
silverZZ0124 <72031018+silverZZ0124@users.noreply.github.com>
2023-12-27 16:26:23 +0900

    [Feat] - 글쓰기 기능 추가

--- src/pages/main/pages/portfolio/component/text-bundle/TextBundle.tsx
+++ src/pages/main/pages/portfolio/component/text-bundle/TextBundle.tsx
@@ -1,37 +1,34 @@
 import React, { useEffect, useState } from "react";
 import styles from "./TextBundle.module.scss";
+import TextAreaAutoResize from "react-textarea-autosize";
 
-const TextBundle: React.FC<{ original?: boolean }> = ({ original = true }) => {
+const TextBundle: React.FC<{
+  original?: boolean;
+  onEnterPressed?: () => void;
+  onValueDeleted?: () => void;
+}> = ({ original = true, onEnterPressed = null, onValueDeleted = null }) => {
   const [value, setValue] = useState<string>("");
-  const [rows, setRows] = useState<number>(1);
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (!e.target.value.includes("\n")) {
-      setValue(e.target.value);
+
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (!original && onEnterPressed !== null) {
+        onEnterPressed!();
+      }
     }
   };
 
-  useEffect(() => {
-    const calculateRows = () => {
-      // 한 줄에 30글자로 가정
-      const charsPerLine = 49;
-
-      // 입력된 텍스트의 길이에 따라 필요한 줄 수 계산
-      const calculatedRows = Math.ceil(value.length / charsPerLine);
-
-      // 계산된 줄 수를 업데이트
-      setRows(calculatedRows || 1);
-    };
-
-    calculateRows();
-  }, [value]);
-
   return (
     <>
-      <textarea
-        value={value}
-        onChange={handleChange}
-        className={`${styles.input} ${original ? styles.original : null}`}
-        rows={rows}
+      <TextAreaAutoResize
+        defaultValue={value}
+        placeholder={
+          original ? "원본 문장을 입력해주세요" : "번역된 문장을 입력해주세요"
+        }
+        autoFocus={original ? true : false}
+        className={`${styles.textarea} ${original ? styles.original : null}`}
+        onKeyDown={keyDownHandler}
       />
     </>
   );
--- src/pages/main/pages/portfolio/component/writing-content/WritingContent.tsx
+++ src/pages/main/pages/portfolio/component/writing-content/WritingContent.tsx
@@ -1,13 +1,66 @@
+import { useEffect, useState } from "react";
 import TextBundle from "../text-bundle/TextBundle";
 import styles from "./WritingContent.module.scss";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const WritingContent = () => {
+  const [rows, setRows] = useState<number>(1);
+  const [children, setChildren] = useState<any[]>([
+    <div className={styles.textBundleContainer}>
+      <div className={styles.textBundleSection}>
+        <TextBundle />
+        <TextBundle
+          original={false}
+          onEnterPressed={() => {
+            setRows((prev) => prev + 1);
+          }}
+        />
+      </div>
+      <div className={styles.deleteIconSection}>
+        <IoMdCloseCircleOutline className={styles.deleteIcon} />
+      </div>
+    </div>,
+  ]);
+
+  const handleDeleteIconClick = (targetIndex: number) => {
+    console.log(targetIndex);
+    setChildren((prev) => {
+      const updatedChildren = [...prev];
+      updatedChildren.splice(targetIndex, 1);
+      return updatedChildren;
+    });
+  };
+
+  useEffect(() => {
+    if (rows === 1) {
+      return;
+    }
+
+    setChildren((prev) => [
+      ...prev,
+      <div className={styles.textBundleContainer}>
+        <div className={styles.textBundleSection}>
+          <TextBundle />
+          <TextBundle
+            original={false}
+            onEnterPressed={() => {
+              setRows((prev) => prev + 1);
+            }}
+          />
+        </div>
+        <div className={styles.deleteIconSection}>
+          <IoMdCloseCircleOutline
+            className={styles.deleteIcon}
+            onClick={() => handleDeleteIconClick(prev.length)}
+          />
+        </div>
+      </div>,
+    ]);
+  }, [rows]);
+
   return (
     <>
-      <div className={styles.container}>
-        <TextBundle />
-        <TextBundle original={false} />
-      </div>
+      <div className={styles.container}>{children}</div>
     </>
   );
 };
