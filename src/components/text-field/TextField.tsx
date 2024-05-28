804911d5a76b12a4cae62d368954b5b6af47f209
local_devkimqwer <dev.kim.qwer@gmail.com>
2024-05-28 17:15:22 +0900

    [Fix] - TextField와 TextFieldAnnotation 통합

--- src/components/text-field/TextField.tsx
+++ src/components/text-field/TextField.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import styles from "./TextField.module.scss";
 
+/**
+ * - Ok - 값이 유효함
+ * - Error - 유효성 검사 실패
+ */
+export type ValidationResult = "Ok" | "Error";
+
 const TextField: React.FC<{
   value: string;
   onChange: (value: string) => void;
   hide?: boolean;
   placeholder?: string;
-}> = ({ value, onChange, hide = false, placeholder = "" }) => {
+  validationFn?: (value: string) => ValidationResult;
+  validationFailText?: string | { (value: string): string };
+}> = ({
+  value,
+  onChange,
+  hide = false,
+  placeholder = "",
+  validationFn = () => "Ok",
+  validationFailText = "값이 유효하지 않습니다.",
+}) => {
   return (
     <>
-      <div className={styles.container}>
+      <div
+        className={`${styles.container}${
+          validationFn(value) === "Error" ? ` ${styles.error}` : ""
+        }`}
+      >
         <input
           className={styles.input}
           type={hide ? "password" : "text"}
@@ -17,6 +36,13 @@ const TextField: React.FC<{
           onChange={(e) => onChange(e.target.value)}
           placeholder={placeholder}
         />
+        {validationFn(value) === "Error" ? (
+          <div className={styles.errorText}>
+            {typeof validationFailText === "string"
+              ? validationFailText
+              : validationFailText(value)}
+          </div>
+        ) : null}
       </div>
     </>
   );
