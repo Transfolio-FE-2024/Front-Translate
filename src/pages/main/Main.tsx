f2b1365168f4954c8a6dffb9b65e0c032c11cbd8
bluebird999280 <bluebird999280@gmail.com>
2024-05-29 06:41:32 +0900

    [Feat] home페이지에서 completion 페이지 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -5,21 +5,23 @@ import Content from "./pages/content/Content";
 import Writer from "./pages/writer/Writer";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Layout from "@/components/layout/Layout";
+import Completion from "./pages/completion/Completion";
 
 const Main = () => {
-  return (
-    <>
-      <Layout>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/content" element={<Content />} />
-          <Route path="/writer" element={<Writer />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/content" element={<Content />} />
+					<Route path="/writer" element={<Writer />} />
+					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/completion" element={<Completion />} />
+				</Routes>
+			</Layout>
+		</>
+	);
 };
 
 export default Main;
