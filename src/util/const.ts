8feadc2ab153fb4199c7029c0cd8b51b189949ad
local_devkimqwer <dev.kim.qwer@gmail.com>
2024-05-28 17:38:02 +0900

    [Chore] - 공통으로 사용되는 상수 추가

---
+++ src/util/const.ts
@@ -0,0 +1,13 @@
+export const TF = {
+  KEY: Object.freeze({
+    SESSION_STORAGE: Object.freeze({
+      SIGNUPINFO: "TF_SIGNUPINFO",
+    }),
+    COOKIE: Object.freeze({
+      TOKEN: "TF_TOKEN",
+    }),
+  }),
+  HTTP_STATUS: Object.freeze({
+    FAIL_UNKNOWN_STATUS: "900",
+  }),
+};
