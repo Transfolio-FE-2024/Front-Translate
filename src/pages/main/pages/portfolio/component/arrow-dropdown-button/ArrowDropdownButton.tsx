6b0a5ebe5b3a857bdb333a1f98272629ca80d0cf
bluebird999280 <bluebird999280@gmail.com>
2024-05-30 21:11:45 +0900

    [Feat] portfolio 작성 시 글자 크기 및 서체 설정 드랍다운에서 스타일 즉시 반영하기

--- src/pages/main/pages/portfolio/component/arrow-dropdown-button/ArrowDropdownButton.tsx
+++ src/pages/main/pages/portfolio/component/arrow-dropdown-button/ArrowDropdownButton.tsx
@@ -1,67 +1,64 @@
 import React from "react";
 import styles from "./ArrowDropdownButton.module.scss";
 
-const ArrowDropdownButton: React.FC<{
-  title: string;
-  values: string[];
-  selectedValue: string | null;
-  onValueClicked: (value: string) => void;
-  isFontFamily?: boolean;
-}> = ({
-  title,
-  values,
-  selectedValue,
-  onValueClicked,
-  isFontFamily = true,
-}) => {
-  const dropdownContentClickHandler = (value: string) => {
-    onValueClicked(value);
-  };
+type StyleType = {
+	[key: string]: {
+		[cssKey: string]: string;
+	};
+};
 
-  return (
-    <>
-      <div className="btn-group">
-        <button
-          className={`btn dropdown-toggle ${styles.button}`}
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          {title}
-        </button>
-        {isFontFamily && (
-          <ul className="dropdown-menu">
-            {values.map((value, index) => (
-              <li
-                className={`dropdown-item ${styles.dropdownFontContent} ${
-                  selectedValue === value ? styles.active : null
-                }`}
-                onClick={() => dropdownContentClickHandler(value)}
-                key={index}
-              >
-                {value}
-              </li>
-            ))}
-          </ul>
-        )}
-        {!isFontFamily && (
-          <ul className="dropdown-menu">
-            {values.map((value, index) => (
-              <li
-                className={`dropdown-item ${styles.dropdownContent} ${
-                  selectedValue === value ? styles.active : null
-                }`}
-                onClick={() => dropdownContentClickHandler(value)}
-                key={index}
-              >
-                {value}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </>
-  );
+const style: StyleType = {
+	"14pt": { fontSize: "14px" },
+	"16pt": { fontSize: "16px" },
+	"18pt": { fontSize: "18px" },
+	"20pt": { fontSize: "20px" },
+	Pretendard: { fontFamily: "Pretendard" },
+	"Nanum Myeangjo": { fontFamily: "NanumMyeongjo" },
+	"Noto sans": { fontFamily: "NotoSans" },
+	"Nanum Barun Gothic": { fontFamily: "NanumBarunGothic" },
+};
+
+const ArrowDropdownButton: React.FC<{
+	title: string;
+	values: string[];
+	selectedValue: string | null;
+	onValueClicked: (value: string) => void;
+}> = ({ title, values, selectedValue, onValueClicked }) => {
+	const dropdownContentClickHandler = (value: string) => {
+		onValueClicked(value);
+	};
+
+	return (
+		<>
+			<div className="btn-group">
+				<button
+					className={`btn dropdown-toggle ${styles.button}`}
+					type="button"
+					data-bs-toggle="dropdown"
+					aria-expanded="false"
+				>
+					{title}
+				</button>
+				<ul className="dropdown-menu">
+					{values.map((value, index) => (
+						<li
+							style={
+								Object.keys(style).includes(value)
+									? style[value]
+									: {}
+							}
+							className={`dropdown-item ${styles.dropdownContent} 
+                ${selectedValue === value ? styles.active : null}`}
+							onClick={() => dropdownContentClickHandler(value)}
+							key={index}
+						>
+							{value}
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default ArrowDropdownButton;
