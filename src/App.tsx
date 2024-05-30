bd1719915af99114ac87f2a215ffc8596cf3f141
bluebird999280 <bluebird999280@gmail.com>
2024-05-30 18:59:49 +0900

    [Fix] mixins.scss 내부에 global.scss를 호출함에도 불구하고 mixins가 여러번 불러들여져 global.scss가 반복적으로 호출됨

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,27 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import "@/styles/global.scss";
 
 const queryClient = new QueryClient();
 
 // 경로 바뀔 때 스크롤 지점 최상단으로 이동
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-  return null;
+	return null;
 };
 
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ScrollToTop />
-      <Router />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ScrollToTop />
+			<Router />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
