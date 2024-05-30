3a6ee1c11c1e559a05c2f5ab0983440689d9ad9c
local_devkimqwer <dev.kim.qwer@gmail.com>
2024-05-30 17:18:10 +0900

    [Design] - 스타일 수정, 반응형 적용, Fix #2

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -4,24 +4,24 @@ import Home from "./pages/home/Home";
 import Content from "./pages/content/Content";
 import Writer from "./pages/writer/Writer";
 import Portfolio from "./pages/portfolio/Portfolio";
-import Layout from "@/components/layout/Layout";
+import Layout from "@/components/Layout/Layout";
 import Completion from "./pages/completion/Completion";
 
 const Main = () => {
-	return (
-		<>
-			<Layout>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/content" element={<Content />} />
-					<Route path="/writer" element={<Writer />} />
-					<Route path="/portfolio" element={<Portfolio />} />
-					<Route path="/completion" element={<Completion />} />
-				</Routes>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/content" element={<Content />} />
+          <Route path="/writer" element={<Writer />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/completion" element={<Completion />} />
+        </Routes>
+      </Layout>
+    </>
+  );
 };
 
 export default Main;
