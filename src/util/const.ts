f995f76fee3a80f13135b533a46072c7ad75bb9b
bluebird999280 <bluebird999280@gmail.com>
2024-05-31 15:21:45 +0900

    [Feat] util/const에 언어 목록, 폰트 사이즈 목록, 폰트 패밀리 목록 추가

--- src/util/const.ts
+++ src/util/const.ts
@@ -1,13 +1,35 @@
 export const TF = {
-  KEY: Object.freeze({
-    SESSION_STORAGE: Object.freeze({
-      SIGNUPINFO: "TF_SIGNUPINFO",
-    }),
-    COOKIE: Object.freeze({
-      TOKEN: "TF_TOKEN",
-    }),
-  }),
-  HTTP_STATUS: Object.freeze({
-    FAIL_UNKNOWN_STATUS: "900",
-  }),
+	KEY: Object.freeze({
+		SESSION_STORAGE: Object.freeze({
+			SIGNUPINFO: "TF_SIGNUPINFO",
+		}),
+		COOKIE: Object.freeze({
+			TOKEN: "TF_TOKEN",
+		}),
+	}),
+	HTTP_STATUS: Object.freeze({
+		FAIL_UNKNOWN_STATUS: "900",
+	}),
+};
+
+export const languageList = ["한국어", "영어", "일본어", "아랍어", "불어"];
+export const fontSizeList = ["14pt", "16pt", "18pt", "20pt"];
+export const fontFamilyList: fontFamilyType[] = [
+	"Pretendard",
+	"Nanum Myeangjo",
+	"Noto Sans",
+	"Nanum Barun Gothic",
+];
+
+export type fontFamilyType =
+	| "Pretendard"
+	| "Nanum Myeangjo"
+	| "Noto Sans"
+	| "Nanum Barun Gothic";
+
+export const fontFamily = {
+	Pretendard: "Pretendard",
+	"Nanum Myeangjo": "NanumMyeangjo",
+	"Noto Sans": "NotoSans",
+	"Nanum Barun Gothic": "NanumBarunGothic",
 };
