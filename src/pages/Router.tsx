d64d6ee45a0f1b637196e17f6d23819a9164531d
local_devkimqwer <dev.kim.qwer@gmail.com>
2024-06-11 21:01:20 +0900

    [Fix] Error 화면 처리를 위해 createBrowserRouter로 변경

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,28 +1,89 @@
-import { Routes, Route } from "react-router-dom";
-import Complete from "./auth/sign-up/pages/complete/Complete";
-import Interests from "./auth/sign-up/pages/interests/Interests";
-import RegForm from "./auth/sign-up/pages/reg-from/RegForm";
+import { createBrowserRouter } from "react-router-dom";
 import Start from "./start/Start";
-import KCallback from "@/components/Oauth/KCallback";
-import Main from "./main/Main";
+import RegForm from "./auth/sign-up/pages/reg-from/RegForm";
+import Interests from "./auth/sign-up/pages/interests/Interests";
 import SignIn from "./auth/sign-in/SignIn";
+import KCallback from "@/components/Oauth/KCallback";
+import Home from "./main/pages/home/Home";
+import Edit from "./main/pages/edit/Edit";
+import Content from "./main/pages/content/Content";
+import Writer from "./main/pages/writer/Writer";
+import Portfolio from "./main/pages/portfolio/Portfolio";
+import Root from "./Root";
+import Complete from "./auth/sign-up/pages/complete/Complete";
+import Main from "./main/Main";
+import DefaultErrorBoundary from "./error/default";
 
-export default function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Start />} />
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <DefaultErrorBoundary />,
+    children: [
+      {
+        path: "/",
+        element: <Start />,
+      },
+      // 회원가입
+      {
+        path: "signup",
+        children: [
+          {
+            path: "regform",
+            element: <RegForm />,
+          },
+          {
+            path: "interests",
+            element: <Interests />,
+          },
+          {
+            path: "complete",
+            element: <Complete />,
+          },
+        ],
+      },
+      // 로그인
+      {
+        path: "signin",
+        element: <SignIn />,
+      },
+      {
+        path: "oauth/kakaocallback",
+        element: <KCallback />,
+      },
+      // 메인 페이지
+      {
+        path: "home",
+        element: <Main />,
+        children: [
+          {
+            path: "",
+            element: <Home />,
+          },
+          {
+            path: "edit",
+            element: <Edit />,
+          },
+          {
+            path: "content",
+            element: <Content />,
+          },
+          {
+            path: "writer/:writerId",
+            element: <Writer />,
+          },
+          {
+            path: "portfolio",
+            element: <Portfolio />,
+          },
+          {
+            path: "completion",
+            element: <Complete />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-      {/* 회원가입 */}
-      <Route path="/signup/regform" element={<RegForm />} />
-      <Route path="/signup/interests" element={<Interests />} />
-      <Route path="/signup/complete" element={<Complete />} />
-
-      {/* 로그인 */}
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/oauth/kakaocallback" element={<KCallback />} />
-
-      {/* 메인 페이지 */}
-      <Route path="/home/*" element={<Main />} />
-    </Routes>
-  );
-}
+export default router;
