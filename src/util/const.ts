fb7da8d09f45bd270825e3a30e36a40075d87218
bluebird999280 <bluebird999280@gmail.com>
2024-06-14 15:59:59 +0900

    [Fix] 개발환경과 배포환경에서의 카카오 로그인 인증 URL 환경변수 설정

--- src/util/const.ts
+++ src/util/const.ts
@@ -12,6 +12,8 @@ export const TF = {
 	}),
 };
 
+export const CLIENT_SITE_ADDRESS = import.meta.env.DEV ? "http://localhost:5173/" : "https://dev--transfolio.netlify.app/";
+
 export const areaOfInterest = {
 	언어: ["한국어", "영어", "일본어", "중국어", "러시아어", "불어", "기타"],
 	전공: ["공학", "교육", "사회", "예체능", "의약", "인문", "자연"],
