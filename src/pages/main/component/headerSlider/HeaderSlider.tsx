1c3c8483964813c435c408d5f3ee0d4add72db79
local_devkimqwer <dev.kim.qwer@gmail.com>
2024-10-21 17:23:26 +0900

    [Design] HeaderSlider 스타일 일부 수정

--- src/pages/main/component/headerSlider/HeaderSlider.tsx
+++ src/pages/main/component/headerSlider/HeaderSlider.tsx
@@ -1,11 +1,12 @@
 import styles from "./HeaderSlider.module.scss";
-import Slider from "react-slick";
+import Slider, { Settings } from "react-slick";
 import mainImg1 from "../../../../assets/images/main_1.png";
 import mainImg2 from "../../../../assets/images/main_2.png";
 import mainImg3 from "../../../../assets/images/main_3.png";
+import { className } from "@/util";
 
 const HeaderSlider = () => {
-  const sliderSettings = {
+  const sliderSettings: Settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -14,19 +15,22 @@ const HeaderSlider = () => {
     arrows: false,
     autoplay: true,
     autoplaySpeed: 10000,
+    accessibility: false, // focus 제거
+    className: styles.slider,
+    dotsClass: className("slick-dots", styles.dots),
   };
 
   return (
     <div className={styles.container}>
       <Slider {...sliderSettings}>
-        <div className={styles.sliderContent}>
-          <img src={mainImg1} className={styles.img} />
+        <div className={styles.customSliderContent}>
+          <img src={mainImg1} className={styles.customSliderImg} />
         </div>
-        <div className={styles.sliderContent}>
-          <img src={mainImg2} className={styles.img} />
+        <div className={styles.customSliderContent}>
+          <img src={mainImg2} className={styles.customSliderImg} />
         </div>
-        <div className={styles.sliderContent}>
-          <img src={mainImg3} className={styles.img} />
+        <div className={styles.customSliderContent}>
+          <img src={mainImg3} className={styles.customSliderImg} />
         </div>
       </Slider>
     </div>
