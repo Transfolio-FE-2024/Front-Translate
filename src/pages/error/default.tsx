c70442ad6b28edc73ac7ef8049ccbceccda368b8
local_devkimqwer <dev.kim.qwer@gmail.com>
2025-02-03 23:15:37 +0900

    [Chore] Error 화면에 레이아웃 적용

--- src/pages/error/default.tsx
+++ src/pages/error/default.tsx
@@ -1,31 +1,43 @@
+import { AuthContext } from "@/context/AuthContext";
 import styles from "./default.module.scss";
 import { TF } from "@/util/const";
+import { useContext } from "react";
 import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Layout from "@/components/Layout/Layout";
+import { Header } from "@/components/Header/Header";
 
 export default function DefaultErrorBoundary() {
   const error = useRouteError();
+  const authContext = useContext(AuthContext);
+  let errorComponent = <ErrorElement_Unknown />;
 
   if (isRouteErrorResponse(error)) {
-    if (error.status === 404) {
-      return <ErrorElement_404 />;
-    }
-
-    if (error.status === 401) {
-      return <ErrorElement_401 />;
-    }
-
-    if (error.status === 503) {
-      return <ErrorElement_503 />;
-    }
-
-    if (error.status === 418) {
-      return <ErrorElement_418 />;
+    switch (error.status) {
+      case 404:
+        errorComponent = <ErrorElement_404 />;
+        break;
+      case 401:
+        errorComponent = <ErrorElement_401 />;
+        break;
+      case 503:
+        errorComponent = <ErrorElement_503 />;
+        break;
+      case 418:
+        errorComponent = <ErrorElement_418 />;
+        break;
     }
   }
 
-  if (error == TF.PAGE_ERROR.NOT_FOUND) return <ErrorElement_404 />;
+  if (error == TF.PAGE_ERROR.NOT_FOUND) {
+    errorComponent = <ErrorElement_404 />;
+  }
 
-  return <ErrorElement_Unknown />;
+  return (
+    <Layout>
+      {authContext.isSignedIn && <Header />}
+      {errorComponent}
+    </Layout>
+  );
 }
 
 //
