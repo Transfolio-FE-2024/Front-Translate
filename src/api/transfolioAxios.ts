11bd72bb8fe9d10dbafd34257fbc8433329abc96
local_devkimqwer <dev.kim.qwer@gmail.com>
2025-04-27 23:03:44 +0900

    [Feat] API 401 응답 시 로그인 페이지로 이동

--- src/api/transfolioAxios.ts
+++ src/api/transfolioAxios.ts
@@ -5,4 +5,19 @@ const transfolioAxios = axios.create({
   withCredentials: true,
 });
 
+transfolioAxios.interceptors.response.use(
+  (response) => {
+    // 정상 응답은 그대로 리턴
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      alert("로그인이 필요합니다.");
+      window.location.href = "/signin";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default transfolioAxios;
